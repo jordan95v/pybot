0bf6316670cf6a90c38cde512f21fc0866e2a23e
jordan95v <jordan.dfrsne@gmail.com>
2024-04-11 18:12:33 +0200

    Added models.

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Server(models.Model):
+    discord_id = models.BigIntegerField(unique=True, null=False)
+    is_open = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    class Meta:
+        unique_together = ("discord_id", "server")
+
+    discord_id = models.BigIntegerField(unique=True, null=False)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    class_name = models.CharField(max_length=10, null=False)
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="students"
+    )
